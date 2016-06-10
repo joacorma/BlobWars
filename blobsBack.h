89bf58bf05997fca6c3ea564d1ac756f55e4dabd
DiegoOrlando17 <diegoorlando.1997@gmail.com>
2016-06-10 11:49:34 -0300

    Update blobsBack.h

--- blobsBack.h
+++ blobsBack.h
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <math.h>
 #include <ctype.h>
+#include "blobsBackV4.h"
 
 #define SAVE 0
 #define FILENAME 1
@@ -11,35 +12,6 @@
 #define GENERAR_COORDENADA(x,y) (x*100 + y)
 #define GENERAR_FILA(x) (floor(x/100))
 #define GENERAR_COLUMNA(x) (x%100)
-
-typedef struct
-	{	
-		int filas;
-		int columnas;
-		int **matriz;
-		int opcion;
-		int turno;
-	} tipoMatriz;
-
-
-int ** CrearTablero(tipoMatriz *Tablero);
-void EjecutarJugada(tipoMatriz *Tablero, int *mov);
-int ValidarParametros(tipoMatriz *Tablero, char **movimiento, int *mov, char **vecErrores);
-int CalcularDistancia(int u, int x, int v, int y);
-void CrearBlob(tipoMatriz *Tablero ,int fila, int columna);
-void Adyacentes(tipoMatriz *Tablero, int fila, int columna);
-void Salto(tipoMatriz *Tablero, int *mov);
-int Fin(tipoMatriz *Tablero);
-void LLenarTablero(tipoMatriz *Tablero);
-int ContarBlobs(tipoMatriz *Tablero);
-int ValidarSave(char **movimiento);
-int RecuperarJuego(tipoMatriz *Tablero, char **vecErrores);
-int JugadaComputadora (tipoMatriz *Tablero, int *mov);
-char * leerCaracteres ();/*hay que sacarlo de aca*/
-void ImprimirError (char **vecErrores, int nroError);/*hay que sacarla de aca, y ponerla en front*/
-void Save(tipoMatriz *Tablero, char  *movimiento, char *idJugador);
-int JugadaComputadora(tipoMatriz *Tablero, int *mov);
-int ContarFichas(tipoMatriz *Tablero, int jugador);
 
 int ** CrearTablero(tipoMatriz *Tablero)
 {
