7e201e254d489e21ab386e97e9091f57a7ca7ece
DiegoOrlando17 <diegoorlando.1997@gmail.com>
2016-06-13 23:36:41 -0300

    UPDATE FINAL DEFINITIVO

--- blobsBack.c
+++ blobsBack.c
@@ -6,9 +6,9 @@
 #include <ctype.h>
 #include "blobsBack.h"
 
-int ** AlocarTablero(tipoMatriz *Tablero)
+int ** AlocarTablero(tipoMatriz *Tablero) 												/* Crea espacio en memoria para el tablero */
 {
-	int i, j, **matrix;
+	int i, **matrix;
 	
 	matrix = malloc(Tablero->filas*sizeof(int*));
 	
@@ -20,23 +20,23 @@ int ** AlocarTablero(tipoMatriz *Tablero)
 	return matrix;
 }
 
-void TableroInicial (tipoMatriz *Tablero, char *contenidoTablero)
+void TableroInicial (tipoMatriz *Tablero, const char *contenidoTablero) 							/* Crea el tablero inicial */
 {
-	int i, j, k;
+	int i, j, k = 0;
 
-	if (contenidoTablero == NULL)
+	if (contenidoTablero == NULL) 								/* Si contenidoTablero es null es porque no es juego recuperado */
 	{
-		for (i=0; i<Tablero->filas; i++)
+		for (i=0; i<Tablero->filas; i++)														/* Llena la matriz con ceros */
 		{
 			for (j=0; j<Tablero->columnas; j++)
 				Tablero->matriz[i][j] = 0;
 		}
-		Tablero->matriz[0][0] = 1;
+		Tablero->matriz[0][0] = 1; 											/* Pone las letras correspondientes en las esquinas */
 		Tablero->matriz[0][(Tablero->columnas)-1] = 2;
 		Tablero->matriz[(Tablero->filas)-1][0] = 1;
 		Tablero->matriz[(Tablero->filas)-1][(Tablero->columnas)-1] = 2;
 	}
-	else
+	else 													/* Si entra en este caso, llena el tablero con el juego que recupero */
 	{
 		for (i=0; i<Tablero->filas; i++)
 		{
@@ -49,7 +49,7 @@ void TableroInicial (tipoMatriz *Tablero, char *contenidoTablero)
 	}
 }
 
-void EjecutarJugada(tipoMatriz *Tablero, int *mov)
+void EjecutarJugada(tipoMatriz *Tablero, int *mov)								/* Ejecutas las jugada y se fija de comer adyacentes */
 {		
 		if (CalcularDistancia(mov[0], mov[1], mov[2], mov[3]) == 1)
 			CrearBlob(Tablero, mov[2], mov[3]);
@@ -59,7 +59,7 @@ void EjecutarJugada(tipoMatriz *Tablero, int *mov)
 		Adyacentes(Tablero, mov[2], mov[3]);
 }
 
-int ValidarParametros(tipoMatriz *Tablero, char **movimiento, int *mov, char **vecErrores, int *error)
+int ValidarParametros(tipoMatriz *Tablero, char **movimiento, int *mov, int *error)
 																									/* Valida la entrada del usuario */
 {
 	int cantLeidos;
@@ -101,7 +101,7 @@ int ValidarParametros(tipoMatriz *Tablero, char **movimiento, int *mov, char **v
 	return 1; 																				/* Retorna 1 si el movimiento es valido */
 }
 
-int ValidarSave(char **movimiento)
+int ValidarSave(char **movimiento) 												/* Valida que se haya entrado un save y un filename */
 {
 	char * save = "save ";
 	int estado = SAVE, i = 0, j = 0;
@@ -114,26 +114,26 @@ int ValidarSave(char **movimiento)
 			{
 				if (save[i] == 0)
 					estado = FILENAME;
-				else if ((*movimiento)[i] == save[i])
+				else if ((*movimiento)[i] == save[i])						/* Compara el vector con otro que tiene la palbra save */
 					i++;
 				else
-					return 1;
+					return 1;													/* Si no es igual sale diciendo que no es un save */
 			}
 			break;
 
-			case FILENAME:
-				(*movimiento)[j++] = (*movimiento)[i++];
+			case FILENAME:													/* Si es un save todo lo que le sigue es un filename */
+				(*movimiento)[j++] = (*movimiento)[i++];							/* Deja en movimiento unicamente el filename */
 			break;
 		}
 	}
 	(*movimiento)[j++] = 0;
 	*movimiento = realloc (*movimiento, j*sizeof(char));
 	if (estado != SAVE)
-		return 0;
+		return 0;																/* Devuelve 0 si el formato del save es valido */
 	return 1;
 }
 
-int ValidarLimites (tipoMatriz *Tablero, int *mov)
+int ValidarLimites (tipoMatriz *Tablero, const int *mov)	/* Comprueba que el movimiento no se pase de los limites del tablero */
 {
 	int i; 
 
@@ -155,7 +155,7 @@ int ValidarLimites (tipoMatriz *Tablero, int *mov)
 	return 0;
 }
 
-int CalcularDistancia(int u, int x, int v, int y)
+int CalcularDistancia(int u, int x, int v, int y) 	/* Calcula la distancia entre la poscicion origen y la poscicion destino */
 {
 	int distFila, distColumna;
 	distFila = abs(v - u);
@@ -201,7 +201,7 @@ void Salto (tipoMatriz *Tablero, int *mov)
 	Tablero->matriz[mov[2]][mov[3]] = aux;	
 }
 
-void Adyacentes (tipoMatriz *Tablero, int fila, int columna)
+void Adyacentes (tipoMatriz *Tablero, int fila, int columna)			/* Come las fichas adyacentes a la poscicion destino */
 {
 	int i, j;
 
@@ -218,7 +218,7 @@ void Adyacentes (tipoMatriz *Tablero, int fila, int columna)
 	}
 }
 
-int Fin (tipoMatriz *Tablero)
+int Fin (tipoMatriz *Tablero)						/* Comprueba que haya al menos un movimiento para el jugador actual */
 {
 	int i, j, k, l;
 
@@ -249,7 +249,7 @@ int Fin (tipoMatriz *Tablero)
 	return 1;
 }
 
-void LLenarTablero (tipoMatriz *Tablero)
+void LLenarTablero (tipoMatriz *Tablero)									/* Llena el tablero al final del juego */
 {
 	int i, j;
 
@@ -263,7 +263,7 @@ void LLenarTablero (tipoMatriz *Tablero)
 	}
 }
 
-int ContarFichas(tipoMatriz *Tablero, int jugador)
+int ContarFichas(tipoMatriz *Tablero, int jugador)					/* Cuenta las fichas actuales de un jugador */
 {
 	int i, j, cantidad = 0;
 
@@ -279,7 +279,7 @@ int ContarFichas(tipoMatriz *Tablero, int jugador)
 	return cantidad;
 }
 
-int JugadaComputadora (tipoMatriz *Tablero, int *mov)
+int JugadaComputadora (tipoMatriz *Tablero, int *mov) 							/* Algoritmo de la computadora */
 {
 	int i, j, k, l, n, m, comidas, comidasMax = 0, posOrigen = -1, posDestino = -1, coin, distancia = 2, cambio;
 
@@ -287,7 +287,7 @@ int JugadaComputadora (tipoMatriz *Tablero, int *mov)
 	{
 		for (j=0; j<Tablero->columnas; j++)
 		{
-			if (Tablero->matriz[i][j] == 2)
+			if (Tablero->matriz[i][j] == 2)						/* comprueba que sea ficha de la computadora */
 			{
 				for (k=(i-2); k<=(i+2); k++)
 				{
@@ -299,7 +299,7 @@ int JugadaComputadora (tipoMatriz *Tablero, int *mov)
 							{	
 								if (Tablero->matriz[k][l] == 0)
 								{
-									comidas = 0;
+									comidas = 0;/* Resetea el contador cada que arranca con una nueva poscicion */
 									for (n=(k-1); n<=(k+1); n++)
 									{
 										if (n>=0 && n<Tablero->filas)
@@ -316,22 +316,25 @@ int JugadaComputadora (tipoMatriz *Tablero, int *mov)
 									}
 									cambio = 0;
 									if (comidas > comidasMax)
+										/* Si encuentra una que come mas la cambia */
 										cambio = 1;
 									else if (comidas == comidasMax && posDestino != -1)
+										/* Si comen lo mismo busca la que tenga menor distancia */
 									{
 										if (distancia > CalcularDistancia(i, j, k, l))
 											cambio = 1;
 										else if (distancia == CalcularDistancia(i, j, k, l))
 										{	
-											coin = rand()%2 + 1;
+											coin = (int) (rand() / ( (double) RAND_MAX + 1) * (2 - 1 + 1) + 1);
+											/* Hace que sea al azar el movimento */
 											if (coin == 1)
 												cambio = 1;
 										}
 									}
-									if (cambio == 1)
+									if (cambio == 1)			/* Si encontro algun mov mejor hace el cambio */
 									{
 										comidasMax = comidas;
-										posOrigen = i*100 + j;
+										posOrigen = i*100 + j;/* Guarda la poscicion como un numero de 4 digitos */
  										posDestino = (k*100 + l);
 										distancia = CalcularDistancia(i, j, k, l);
 
@@ -344,7 +347,7 @@ int JugadaComputadora (tipoMatriz *Tablero, int *mov)
 			}
 		}
 	}
-	if (comidasMax != 0)
+	if (comidasMax != 0) 							/* Si encontro para comer las pone en el vector termina */
 	{
  			mov[0] = GENERAR_FILA(posOrigen);
  			mov[1] = GENERAR_COLUMNA(posOrigen);
@@ -353,7 +356,7 @@ int JugadaComputadora (tipoMatriz *Tablero, int *mov)
  			return 1;
  	}
 
-	coin = 1;
+	coin = 1;												/* Si no encontro para comer busca adyantes */
 	for (i=0; i<Tablero->filas; i++)
 	{
 		for (j=0; j<Tablero->columnas; j++)
@@ -368,10 +371,11 @@ int JugadaComputadora (tipoMatriz *Tablero, int *mov)
 						{
 							if (l>=0 && l<Tablero->columnas)
 							{
-								if (Tablero->matriz[k][l] == 0)
+								if (Tablero->matriz[k][l] == 0)	/* Comprueba que el destino este vacio */
 								{
 									if (posDestino != -1)
-										coin = rand()%2 + 1;
+									/* Si es la primera que encuentra la pone en el vector */
+										coin = (int) (rand() / ( (double) RAND_MAX + 1) * (2 - 1 + 1) + 1);
 									if (coin == 1)
 									{
 										posOrigen = i*100 + j;
@@ -387,7 +391,7 @@ int JugadaComputadora (tipoMatriz *Tablero, int *mov)
 			}
 		}
 	}
-	if (posDestino == -1)
+	if (posDestino == -1) 		/* Si tampoco encontro adyacentes devuelve que no tiene movimientos */
  		return 4;
  	
  	mov[0] = GENERAR_FILA(posOrigen);
